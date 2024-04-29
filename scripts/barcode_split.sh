a1ba8b40c167ebfb25e2fd5c7863cd417e39b88d
Mallory Le Corre <128157909+Mallory56@users.noreply.github.com>
2024-04-29 13:52:42 +0200

    Delete scripts/barcode_split.sh

--- scripts/barcode_split.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-summary_directory="/home/mlecorre/metabarcoding_nanopore/data/sequencing_summary_FAW93216_3bdc7dd0_6a0e5917.txt"
-results_directory="/home/mlecorre/metabarcoding_nanopore/results/summary_barcode_split/"
-
-Barcode_split -f "$summary_directory" -o "$results_directory"
-
